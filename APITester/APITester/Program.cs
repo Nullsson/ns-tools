3ad993c3e60b55c31b834db915c8a07ae962d3cd
Oskar Mendel <brokenprogrammer@gmail.com>
2026-01-01 15:16:20 +0100

    Add boilerplate for test tool

---
+++ APITester/APITester/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.CommandLine;
+
+namespace APITester;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var rootCommand = BuildRootCommand();
+    }
+
+    private static RootCommand BuildRootCommand()
+    {
+        var usersOption = new Option<string>("--user-list", "-u")
+        {
+            Description = "Path to file with list of user credentials.",
+            Required = true,
+        };
+
+        var routesOption = new Option<string>("--routes", "-r")
+        {
+            Description = "Paths to file with list of routes to test.",
+            Required = true,
+        };
+        
+        var rootCommand = new RootCommand("API Testing Tool");
+        rootCommand.Add(usersOption);
+        rootCommand.Add(routesOption);
+        
+        rootCommand.SetAction(async (parseResult, cancellationToken) =>
+        {
+            var userlistPath = parseResult.GetValue(usersOption);
+            var routesPath = parseResult.GetValue(routesOption);
+            
+            return await ExecuteApiTestAsync(userlistPath, routesPath);
+        });
+        
+        return rootCommand;
+    }
+
+    private static async Task<int> ExecuteApiTestAsync(string userlistPath, string routesPath)
+    {
+        return 1;
+    }
+}
